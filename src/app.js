1f38946d01ce8fdd48e3b114187227de99ea936b
626 <qq402284347@gmail.com>
2022-11-28 17:50:21 +0800

    feat(请求转发服务)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
-const express = require('express')
-const app = express()
+const Koa = require('koa');
+const koaStatic = require('koa-static');
+const koaMount = require('koa-mount');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const k2c = require('koa2-connect');
+const json = require('./url.json')
+const resolve = file => path.resolve(__dirname, file);
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const app = new Koa();
+app.use(async (ctx, next) => {
+  const url = ctx.path;
+  if (url.startsWith('/api')) {
+    ctx.respond = false;
+    await k2c(
+      createProxyMiddleware({
+        target: json[0],
+        changeOrigin: true,
+        secure: false,
+      }),
+    )(ctx, next);
+    return await next();
+  }
+  return await next();
+});
+// 开放目录
+app.use(koaMount('/', koaStatic(resolve('../dist'))));
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(` Your application is running here: http://localhost:${port}`);
+});
